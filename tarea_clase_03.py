c3b95babb9f5aec8f87ffa55fe862d2891acda27
Xavi Giron <p-xgiron@neoris.com>
2022-03-31 14:23:12 +0200

    Tarea sesión día 09/09/2021

---
+++ tarea_clase_03.py
@@ -0,0 +1,8 @@
+tuplita = (1, "Mallorca", True, 10, "Xavi")
+lista = list(tuplita)
+dicci = {1:"", 2:"", 3:"", 4:"", 5:"" }
+dicci[1] = lista[0]
+dicci[2] = lista[1]
+dicci[3] = lista[2]
+dicci[4] = lista[3]
+dicci[5] = lista[4]
